855b1239193a0ec905976cc1f24a8c8e6cf51102
Ahmed Atef <43449355+ahmedatefdev@users.noreply.github.com>
2023-11-11 19:41:54 +0200

    update catch error

--- src/app/signup/handleSubmitServerAction.tsx
+++ src/app/signup/handleSubmitServerAction.tsx
@@ -12,27 +12,20 @@ export const handleSubmit = async (e: FormData) => {
     name: username,
   };
   const url = process.env.NEXT_PUBLIC_API_URL + "/auth/signup";
-  try {
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "x-guest": "error",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(bodyData),
-    });
-    const jsonRes = await res.json();
-    if (jsonRes?.statusCode >= 400) {
-      throw new Error(jsonRes?.message);
-    }
-    if (jsonRes.email) {
-      redirect("/signin");
-    }
-  } catch (error: any) {
-    console.log(
-      "🚀 ~ file: handleSubmitServerAction.tsx:30 ~ handleSubmit ~ error:",
-      error.toString()
-    );
-    return JSON.parse(JSON.stringify({ message: error.toString() }));
+  // try {
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "x-guest": "error",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(bodyData),
+  });
+  const jsonRes = await res.json();
+  if (jsonRes?.statusCode >= 400) {
+    return JSON.parse(JSON.stringify({ message: jsonRes.message.toString() }));
+  }
+  if (jsonRes.email) {
+    redirect("/signin");
   }
 };
